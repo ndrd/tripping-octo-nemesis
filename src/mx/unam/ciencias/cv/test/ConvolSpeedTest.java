0f586c29421742914ab7e7fbfcc5c099d25457a6
Jonathan Andrade <ndrd@ciencias.unam.mx>
2015-02-28 00:36:34 -0600

    Adding junit

---
+++ src/mx/unam/ciencias/cv/test/ConvolSpeedTest.java
@@ -0,0 +1,12 @@
+package mx.unam.ciencias.cv.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConvolSpeedTest {
+
+	@Test public void longitud() {
+		Assert.assertTrue(1 == 1);
+	}
+	
+}
---
+++ src/mx/unam/ciencias/cv/test/FiltersTest.java
@@ -0,0 +1,20 @@
+package  mx.unam.ciencias.cv.test;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+    
+public class FiltersTest {
+
+  
+  @Test
+  public void testExceptionIsThrown() {
+    FiltersTest tester = new FiltersTest();
+  }
+
+  @Test
+  public void testMultiply() {
+    FiltersTest tester = new FiltersTest();
+    Assert.assertTrue(1 == 1);
+  }
+} 
